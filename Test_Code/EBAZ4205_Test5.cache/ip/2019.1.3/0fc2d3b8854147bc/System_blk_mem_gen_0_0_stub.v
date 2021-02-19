b2e23ae9139c6a084ff5f6ffeed57e50388e4fab
David Douglas <fasterthanasnail@googlemail.com>
2021-02-19 20:02:31 +0000

    Add files via upload

---
+++ Test_Code/EBAZ4205_Test5.cache/ip/2019.1.3/0fc2d3b8854147bc/System_blk_mem_gen_0_0_stub.v
@@ -0,0 +1,22 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.1.3 (win64) Build 2644227 Wed Sep  4 09:45:24 MDT 2019
+// Date        : Sun Jan 31 13:38:35 2021
+// Host        : DESKTOP-VR9QE9J running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+//               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ System_blk_mem_gen_0_0_stub.v
+// Design      : System_blk_mem_gen_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z010clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "blk_mem_gen_v8_4_3,Vivado 2019.1.3" *)
+module decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix(clka, addra, douta)
+/* synthesis syn_black_box black_box_pad_pin="clka,addra[16:0],douta[15:0]" */;
+  input clka;
+  input [16:0]addra;
+  output [15:0]douta;
+endmodule
---
+++ Test_Code/EBAZ4205_Test5.cache/ip/2019.1.3/0fc2d3b8854147bc/System_blk_mem_gen_0_0_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.1.3 (win64) Build 2644227 Wed Sep  4 09:45:24 MDT 2019
+-- Date        : Sun Jan 31 13:38:35 2021
+-- Host        : DESKTOP-VR9QE9J running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+--               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ System_blk_mem_gen_0_0_stub.vhdl
+-- Design      : System_blk_mem_gen_0_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z010clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix is
+  Port ( 
+    clka : in STD_LOGIC;
+    addra : in STD_LOGIC_VECTOR ( 16 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 15 downto 0 )
+  );
+
+end decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix;
+
+architecture stub of decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,addra[16:0],douta[15:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_3,Vivado 2019.1.3";
+begin
+end;
